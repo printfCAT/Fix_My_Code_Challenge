1b5ece62b7d6ee8705f09e25d7fa71b8f07acdfd
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-22 11:20:54 -0700

    Double linked list

---
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *prev;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	current = *head;
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	while (current != NULL && count < index)
+	{
+		prev = current;
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+	return (-1);
+
+	prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = prev;
+	free(current);
+	return (1);
+}
