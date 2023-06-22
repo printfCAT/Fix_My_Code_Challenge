6d4f4c27ee41464a76642352822e39277e1857e1
RIvAL-411 <cliffomanyo@gmail.com>
2023-06-22 11:30:05 -0700

    Double linked list

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,36 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current, *prev;
-	unsigned int count = 0;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
 	if (*head == NULL)
+	{
 		return (-1);
-
-	current = *head;
-
-	if (index == 0)
-	{
-		*head = current->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
 	}
-
-	while (current != NULL && count < index)
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
 	{
-		prev = current;
-		current = current->next;
-		count++;
+		*head = (*head)->next;
+		p++;
 	}
-
-	if (current == NULL)
-	return (-1);
-
-	prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = prev;
-	free(current);
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
 	return (1);
 }
